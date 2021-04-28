f22f67265564d1b5b1acbe148a2503f37d3cf933
Aryan Akshay <varyanakshay@gmail.com>
2021-04-28 16:43:24 +0530

    Add and update students

--- Springboot/src/main/java/com/example/demo/StudentController.java
+++ Springboot/src/main/java/com/example/demo/StudentController.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,4 +21,22 @@ public class StudentController {
       
         return studentService.getStudents();
     }
+    @RequestMapping("/students/{id}")
+    
+    public Student getStudent(@PathVariable int id ) {
+    	return studentService.getStudent(id);
+    }
+    @RequestMapping(method = RequestMethod.POST, value ="students")
+	public void addStudent(@RequestBody Student student) {
+		System.out.println("controllers add student");
+		studentService.addStudent(student);
+		
+    }
+    @RequestMapping(method = RequestMethod.PUT, value = "students/{id}")
+	public void updateStudent(@RequestBody Student student, @PathVariable int id) {
+		studentService.updateStudent(student,id);
+	}
+    	
+    	
+
 }
--- Springboot/src/main/java/com/example/demo/StudentService.java
+++ Springboot/src/main/java/com/example/demo/StudentService.java
@@ -3,21 +3,44 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Service
 public class StudentService {
 
-    @RequestMapping("students")
-    public List<Student> getStudents(){
-       List<Student> students = new ArrayList<>();
-        students.add(new Student(1, "Aryan", 1, 97));
-        students.add(new Student(2, "Akshay", 3, 98));
-        students.add(new Student(3, "Sanjay", 4, 99));
-        students.add(new Student(4, "Subbu", 5, 100));
 
-        return students;
-    }
+	List<Student> students = new ArrayList<>();
 
+	public void createStudents() {
+		students.add(new Student(1, "Aryan", 1, 97));
+		students.add(new Student(2, "Akshay", 3, 98));
+		students.add(new Student(3, "Sanjay", 4, 99));
+		students.add(new Student(4, "Subbu", 5, 100));
+
+
+	}
+	public List<Student> getStudents(){
+		createStudents();
+		return students;
+	}
+
+	public Student getStudent(int id) {
+		Student student=  students.stream().filter(s -> s.getId() == id ).findFirst().get();
+		return student;
+	}
+	public void addStudent(Student student) {
+		System.out.println("controllers add student");
+		students.add(student);
+	}
+	public void updateStudent(Student student, int id) {
+		for(int i = 0; i< students.size(); i++) {
+			Student s = students.get(0);
+			if(s.getId() == id) {
+				students.set(id, student);
+				return;
+			}
+		}
+	}
 }
